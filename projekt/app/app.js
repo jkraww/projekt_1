2fd52eac39982622fecc83302347dc0a3a7c382e
Justyna Krawczyk <jkrawczyk993@gmail.com>
2017-06-30 09:54:57 +0200

    prawie skonczony ale jeszcze nie

---
+++ projekt/app/app.js
@@ -0,0 +1,188 @@
+function myFunction(hamburger) {
+    hamburger.classList.toggle("change");
+}
+
+
+
+var map = AmCharts.makeChart( "chartdiv", {
+  "type": "map",
+  "theme": "none",
+  "projection": "miller",
+
+  "imagesSettings": {
+    "rollOverColor": "#934b08",
+    "rollOverScale": 3,
+    "selectedScale": 3,
+    "selectedColor": "#af4910",
+    "color": "#aa241b"
+  },
+
+  "areasSettings": {
+    "unlistedAreasColor": "#e89f32"
+  },
+
+  "dataProvider": {
+    "map": "worldLow",
+    "images": [ {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Brussels",
+      "latitude": 50.8371,
+      "longitude": 4.3676
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Copenhagen",
+      "latitude": 55.6763,
+      "longitude": 12.5681
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Paris",
+      "latitude": 48.8567,
+      "longitude": 2.3510
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Reykjavik",
+      "latitude": 64.1353,
+      "longitude": -21.8952
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Moscow",
+      "latitude": 55.7558,
+      "longitude": 37.6176
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Madrid",
+      "latitude": 40.4167,
+      "longitude": -3.7033
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "London",
+      "latitude": 51.5002,
+      "longitude": -0.1262,
+      "url": "http://www.google.co.uk"
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Peking",
+      "latitude": 39.9056,
+      "longitude": 116.3958
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "New Delhi",
+      "latitude": 28.6353,
+      "longitude": 77.2250
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Tokyo",
+      "latitude": 35.6785,
+      "longitude": 139.6823,
+      "url": "http://www.google.co.jp"
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Ankara",
+      "latitude": 39.9439,
+      "longitude": 32.8560
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Buenos Aires",
+      "latitude": -34.6118,
+      "longitude": -58.4173
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Brasilia",
+      "latitude": -15.7801,
+      "longitude": -47.9292
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Ottawa",
+      "latitude": 45.4235,
+      "longitude": -75.6979
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Washington",
+      "latitude": 38.8921,
+      "longitude": -77.0241
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Kinshasa",
+      "latitude": -4.3369,
+      "longitude": 15.3271
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Cairo",
+      "latitude": 30.0571,
+      "longitude": 31.2272
+    }, {
+      "zoomLevel": 5,
+      "scale": 0.5,
+      "title": "Pretoria",
+      "latitude": -25.7463,
+      "longitude": 28.1876
+    } ]
+  }
+} );
+
+
+map.addListener( "positionChanged", updateCustomMarkers );
+
+function updateCustomMarkers( event ) {
+  var map = event.chart;
+
+  for ( var x in map.dataProvider.images ) {
+    var image = map.dataProvider.images[ x ];
+
+
+    if ( 'undefined' == typeof image.externalElement )
+      image.externalElement = createCustomMarker( image );
+    var xy = map.coordinatesToStageXY( image.longitude, image.latitude );
+    image.externalElement.style.top = xy.y + 'px';
+    image.externalElement.style.left = xy.x + 'px';
+  }
+}
+
+
+function createCustomMarker( image ) {
+
+  var holder = document.createElement( 'div' );
+  holder.className = 'map-marker';
+  holder.title = image.title;
+  holder.style.position = 'absolute';
+
+
+  if ( undefined != image.url ) {
+    holder.onclick = function() {
+      window.location.href = image.url;
+    };
+    holder.className += ' map-clickable';
+  }
+
+
+  var dot = document.createElement( 'div' );
+  dot.className = 'dot';
+  holder.appendChild( dot );
+
+
+  var pulse = document.createElement( 'div' );
+  pulse.className = 'pulse';
+  holder.appendChild( pulse );
+
+
+  image.chart.chartDiv.appendChild( holder );
+
+  return holder;
+}
